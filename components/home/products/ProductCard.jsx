0451f683081a0b7e7ef062268999b85881c8309c
Sajib Hossain <sajibhossain1185@gmail.com>
2022-07-01 00:39:37 +0600

    added functionality on image hover icon

--- components/home/products/ProductCard.jsx
+++ components/home/products/ProductCard.jsx
@@ -4,40 +4,47 @@ import StarRatingComponent from 'react-star-rating-component';
 import { AiFillStar } from 'react-icons/ai'
 import Link from 'next/link';
 import { urlFor } from '../../../sanity';
+import { useStateContext } from '../../context/EcommerceContext';
 
 const ProductCard = ({ product }) => {
     const { slug, image, name, price, rating } = product
+    const { onAdd } = useStateContext()
     return (
 
         <>
-            <Link href={`/product/${slug.current}`}>
-                <div className='product__card'>
-                    <div className="product__card__head">
+
+            <div className='product__card'>
+                <div className="product__card__head">
+                    <Link href={`/product/${slug.current}`}>
                         <img src={urlFor(image && image[0])} alt="pro" />
-                        <div className='product__card__cart'>
-                            <button>
-                                <BsHandbag />
-                            </button>
-                        </div>
-                    </div>
-                    <div className="product__card__body">
-                        <div className='product__ratings'>
-                            <StarRatingComponent
-                                name="rating"
-                                editing={false}
-                                renderStarIcon={() => <span><AiFillStar /></span>}
-                                starCount={5}
-                                value={rating}
-                                starColor="#e12c43"
-                            />
-                        </div>
-                        <div className='product__info'>
-                            <h3>{name}</h3>
-                            <span>${price}</span>
-                        </div>
+                    </Link>
+
+                    <div className='product__card__cart' onClick={() => onAdd(product, 1)}>
+                        <button>
+                            <BsHandbag />
+                        </button>
                     </div>
                 </div>
-            </Link>
+                <div className="product__card__body">
+                    <div className='product__ratings'>
+                        <StarRatingComponent
+                            name="rating"
+                            editing={false}
+                            renderStarIcon={() => <span><AiFillStar /></span>}
+                            starCount={5}
+                            value={rating}
+                            starColor="#e12c43"
+                        />
+                    </div>
+                    <div className='product__info'>
+                        <Link href={`/product/${slug.current}`}>
+                            <h3>{name}</h3>
+                        </Link>
+                        <span>${price}</span>
+                    </div>
+                </div>
+            </div>
+
 
         </>
     )
