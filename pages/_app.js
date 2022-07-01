1aa2d6de72d3f7fdfb70b341e69a2f0e2d416346
Sajib Hossain <sajibhossain1185@gmail.com>
2022-07-02 01:59:54 +0600

    added 404 and fixed resposiveness bugs

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,18 @@ import 'swiper/scss/pagination';
 import Header from "../components/global/header/Header"
 import Footer from '../components/global/Footer/Footer'
 import { StateContext } from '../components/context/EcommerceContext'
+import Head from 'next/head'
 
 
 function MyApp({ Component, pageProps }) {
   return <>
     <StateContext>
       <Header />
+      <Head>
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <title>Ecommerceo | Buy Fashinable Clothes</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+      </Head>
       <Component {...pageProps} />
       <Footer />
     </StateContext>
