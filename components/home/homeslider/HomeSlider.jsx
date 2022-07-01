69bf1b369f360ca269617c00c9d07ff2d37f869c
Sajib Hossain <sajibhossain1185@gmail.com>
2022-07-02 00:01:52 +0600

    made homeslider dynamic

--- components/home/homeslider/HomeSlider.jsx
+++ components/home/homeslider/HomeSlider.jsx
@@ -1,13 +1,24 @@
-import Link from 'next/link'
-import React from 'react'
-import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from "swiper";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { sanityClient, urlFor } from "../../../sanity";
 const HomeSlider = () => {
+    const [slider, setSlider] = useState([]);
+
+    async function FetchProducts() {
+        const query = '*[_type == "homeSlider"]';
+        const products = await sanityClient.fetch(query);
+        setSlider(products);
+    }
+
+    useEffect(() => {
+        FetchProducts();
+    }, []);
+
     return (
         <>
-
-
             <div>
                 <Swiper
                     // install Swiper modules
@@ -21,40 +32,33 @@ const HomeSlider = () => {
                         disableOnInteraction: true,
                         waitForTransition: true,
                         pauseOnMouseEnter: true,
-
                     }}
                 >
-                    <SwiperSlide>
-                        <div className="product__slider" style={{ backgroundImage: 'url(https://demo.ishithemes.com/opencart/OPC140/OPC140L01/image/cache/catalog/slider/Slider-1-1920x800.png)' }} >
-                            <div className="product__slider__container container">
-                                <h1>feel pretty feel classy</h1>
-                                <p>
-                                    “Fashion is not something that exists in dresses only. Fashion is in the sky, in the street, fashion has to do with ideas, the way we live, what is happening
-                                </p>
-                                <Link href="/product">
-                                    <a className="button">show now</a>
-                                </Link>
-                            </div>
-                        </div >
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="product__slider" style={{ backgroundImage: 'url(https://demo.ishithemes.com/opencart/OPC140/OPC140L01/image/cache/catalog/slider/Slider-1-1920x800.png)' }} >
-                            <div className="product__slider__container container">
-                                <h1>feel pretty feel classy</h1>
-                                <p>
-                                    “Fashion is not something that exists in dresses only. Fashion is in the sky, in the street, fashion has to do with ideas, the way we live, what is happening
-                                </p>
-                                <Link href="/product">
-                                    <a className="button">show now</a>
-                                </Link>
-                            </div>
-                        </div >
-                    </SwiperSlide>
+                    {slider.map((item) => {
+                        const { image, heading, description } = item;
+                        return (
+                            <>
+                                <SwiperSlide>
+                                    <div
+                                        className="product__slider"
+                                        style={{ backgroundImage: `url(${urlFor(image)})` }}
+                                    >
+                                        <div className="product__slider__container container">
+                                            <h1>{heading}</h1>
+                                            <p>{description}</p>
+                                            <Link href="/product">
+                                                <a className="button">show now</a>
+                                            </Link>
+                                        </div>
+                                    </div>
+                                </SwiperSlide>
+                            </>
+                        );
+                    })}
                 </Swiper>
-
             </div>
         </>
-    )
-}
+    );
+};
 
-export default HomeSlider
+export default HomeSlider;
